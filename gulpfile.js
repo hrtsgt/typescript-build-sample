f3a2dc5988c560b210c8b09db56ad6be8735c255
kanase <shinsk333rs@gmail.com>
2016-03-23 23:11:09 +0900

    fix

--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,14 @@
 // ライブラリの読み込み
 const gulp = require('gulp');
 const sass = require('gulp-sass');
+const cssMin = require('gulp-clean-css');
 const plumber = require('gulp-plumber');
+const runSequence = require('run-sequence');
 const del = require('del');
 const webserver  = require('gulp-webserver');
 const webpack = require('gulp-webpack');
 const webpackConfig = require('./webpack.config.js');
+const env = process.env.NODE_ENV;
 
 // jsとcssの削除
 gulp.task('clean', () => {
@@ -39,6 +42,14 @@ gulp.task('sass', () => {
     .pipe(gulp.dest('./dist/css'));
 });
 
+// css圧縮
+gulp.task('cssMin', () => {
+  gulp.src('./dist/css/*.css')
+    .pipe(plumber())
+    .pipe(cssMin())
+    .pipe(gulp.dest('./dist/css'));
+});
+
 // tsとsassの監視
 gulp.task('watch', () => {
   gulp.watch('./src/**/*.ts', ['webpack']);
@@ -46,4 +57,10 @@ gulp.task('watch', () => {
 });
 
 // デフォルトタスク
-gulp.task('default', ['clean', 'webpack', 'sass', 'watch', 'serve']);
+gulp.task('default', () => {
+  if (env === 'production') {
+    runSequence('clean', ['webpack', 'sass'], 'cssMin');
+  } else {
+    runSequence('clean', ['webpack', 'sass'], 'watch', 'serve');
+  }
+});
